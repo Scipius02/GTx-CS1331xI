74060f4a9753c9009eda5cfa96dac3856378049a
Scipius02 <heltonv@bu.edu>
2024-07-15 16:05:10 -0700

    didn't do this assignment because i've already done it in HS but maybe i'll come back to it at some point out of boredom

---
+++ Battleship.java
@@ -0,0 +1,8 @@
+public class Battleship {
+
+    // nvm i did something like this in hs and i'm too lazy to reproduce
+    public static void main(String[] args) {
+        String welcome = "Welcome to Battleship!";
+        System.out.println(welcome);
+    }
+}
